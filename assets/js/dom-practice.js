64ac9a8622780b6dfe0d2a3ffeaffe3338bba51c
Wayt <01flax01@gmail.com>
2022-02-24 16:47:54 +0200

    Update work

---
+++ assets/js/dom-practice.js
@@ -0,0 +1,36 @@
+const main = document.querySelector('main');
+
+const article = `<article></artile>`;
+
+const article1 = document.createElement('article')
+
+main.append(article1)
+
+
+const img = `<img src="" alt="">`;
+
+article1.append(img)
+
+
+const h3 = `<h3>Отличная погода!</h3>`;
+
+article1.append(h3);
+
+
+const p = `<p>Синоптики обещают хорошую поргоду в течение всего сезона!</p>`;
+
+article1.append(p);
+
+
+const button = `<button>Подробнее</button>`;
+
+article1.append(button);
+
+
+/* 
+const body = ;
+
+for () {
+
+}
+ */
